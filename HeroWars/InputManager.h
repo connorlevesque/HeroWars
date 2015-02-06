f9caa24ffac507bd19bf7761cce1aa230633354a
shashman95 <maxshashoua@gmail.com>
2015-02-05 21:24:46 -0700

    Make input manager log type of touch (drag when touch leaves current node)

---
+++ HeroWars/InputManager.h
@@ -0,0 +1,16 @@
+//
+//  InputManager.h
+//  HeroWars
+//
+//  Created by Max Shashoua on 2/5/15.
+//  Copyright (c) 2015 Max Shashoua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface InputManager : NSObject
+
+-(void)receiveInputWithNode:(SKNode *) node andString: (NSString *) touchType;
+
+@end
